eb8a7e096508db9102c34cb13beeb74b29092e7b
GabyGV <gaby.gutierrez.va@users.noreply.github.com>
2020-05-13 13:15:59 -0600

    avance1

---
+++ src/app/GabyGV/gabycp/gabycp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gabycp',
+  templateUrl: './gabycp.component.html',
+  styleUrls: ['./gabycp.component.css']
+})
+export class GabycpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ExampleComponent } from './example/example.component';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { GabycpComponent } from './GabyGV/gabycp/gabycp.component';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
     AppComponent,
     MainComponent,
     NotFoundComponent,
-    ExampleComponent
+    ExampleComponent,
+    GabycpComponent
   ],
   imports: [
     BrowserModule,
