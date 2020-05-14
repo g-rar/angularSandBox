e7fb322b2517d765c62b6fe27bc80efd2bf97dca
Walter López Araya <wallyjosuelopeza@estudiantestec.cr>
2020-05-13 20:58:03 -0600

    Creación del componente
    Se crea el componente Walter al protecto con la instrucción
     'ng g component Walter'

---
+++ src/app/walter/walter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-walter',
+  templateUrl: './walter.component.html',
+  styleUrls: ['./walter.component.css']
+})
+export class WalterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
