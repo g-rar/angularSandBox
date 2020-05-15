2acece7afa63da45094a164dd36dec33d59c0907
Walter López Araya <wallyjosuelopeza@estudiantestec.cr>
2020-05-15 10:59:26 -0600

    Agrego el componente al side menu
    Se agrega el enlace a WalterComponent en el side menu y se incluye en el app-routing el enlace a WalterComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ExampleComponent } from './example/example.component';
+import { WalterComponent } from './walter/walter.component';
 
 
 const routes: Routes = [
   {path: '', component:MainComponent},
   {path: 'example', component:ExampleComponent},
+  {path: 'walter', component:WalterComponent},
   {path: '**', component:NotFoundComponent}
 ];
 
