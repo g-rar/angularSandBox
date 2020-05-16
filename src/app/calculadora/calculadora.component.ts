ecd886fbeaec984d57a662d23ac13037d2e6e6be
MrLuis137 <47502456+MrLuis137@users.noreply.github.com>
2020-05-16 11:09:46 -0600

    Estilo y corrección de errores
    Listo para darle funcionalidad

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -7,13 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculadoraComponent implements OnInit {
 
-  private displayText :displayText;
+  private displayText :string;
   constructor() {
     this.displayText = '';
    }
 
   private keyPressed(key){
     this.displayText += key;
+  }
+
+  private del(){
+    this.displayText = this.displayText.slice(0, -1);
+  }
+
+  private ac() {
+    this.displayText = '';
+  }
+
+  private calculate() {
+    var elements = this.displayText.split(' ');
+    console.log(elements);
   }
 
   ngOnInit() {
