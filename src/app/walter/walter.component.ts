a16d54db1256652121e88bccde23bed06d67cdd3
Walter López Araya <wallyjosuelopeza@estudiantestec.cr>
2020-05-17 20:16:04 -0600

    Realizar un registro de estudiantes
    Se agrega el código HTML para crear un form para ingresar datos de un estudiante y se crea una clase en el archivo typeScript para los estudiantes

--- src/app/walter/walter.component.ts
+++ src/app/walter/walter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-walter',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WalterComponent implements OnInit {
 
-  constructor() { }
+  inputForm = {};
+
+  constructor(private formBuilder: FormBuilder) {
+    this.inputForm = this.formBuilder.group({
+      nameInputForm: '',
+      lastNameInputForm: '',
+      cellphoneInputForm: '',
+      emailInputFrom: '',
+      carnetInputForm: ''
+    });
+  }
 
   ngOnInit() {
   }
 
+  registerAnUserFromForm(value){
+    let name = value.nameInputForm;
+    let lastName = value.lastNameInputForm;
+    let cellphone = value.cellphoneInputForm;
+    let email = value.emailInputFrom;
+    let carnet = value.carnetInputForm;
+
+    let estudiante = new Estudiante(name, lastName, cellphone, email, carnet);
+  }
+}
+
+class Estudiante {
+  nombre: string;
+  apellido: string;
+  telefono: string;
+  email: string;
+  carnet: string;
+  constructor(pNombre: string, pApellido: string, pTelefono: string, pEmail: string, pCarnet: string){
+    this.nombre = pNombre;
+    this.apellido = pApellido;
+    this.telefono = pTelefono;
+    this.email = pTelefono;
+    this.carnet = pCarnet;
+  }
 }
