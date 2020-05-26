a4a36d451eb26f30595c54b18867690c14c03821
valevqb <2018085308vale>
2020-05-26 14:47:24 -0500

    Botones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,11 @@ import { MainComponent } from './main/main.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ExampleComponent } from './example/example.component';
 import { ValeriaComponent } from './valeria/valeria.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { Component, HostBinding } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 
 const routes: Routes = [
   {path: '', component:MainComponent},
@@ -13,7 +18,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [BrowserAnimationsModule, NoopAnimationsModule, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/valeria/valeria.component.ts
+++ src/app/valeria/valeria.component.ts
@@ -3,15 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-valeria',
   templateUrl: './valeria.component.html',
-  styleUrls: ['./valeria.component.css']
+  styleUrls: ['./valeria.component.css'],
+  animations: []
 })
 export class ValeriaComponent implements OnInit {
   //Titulo
   tittles: "Practica de mis Componentes";
   isCollapsed:boolean = true;
   inputText:string = "";
-  longCalculator: string[] = ['AC','CE'];
-  buttonsCalculator: string[] = ['7','8','9','/','4','5','6','*','1','2','3','-','.','0','=','+'];
 
   constructor() {
   }
